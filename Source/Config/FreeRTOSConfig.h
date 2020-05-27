792ae3e7ed4a13c0f65b075b53e4fca9d2c0d7b6
kongyx <kongyx@feimarobotics.com>
2020-05-27 19:22:26 +0800

    1.add semaphore

--- Source/Config/FreeRTOSConfig.h
+++ Source/Config/FreeRTOSConfig.h
@@ -122,9 +122,8 @@ header file. */
 #define configUSE_MALLOC_FAILED_HOOK		0
 #define configUSE_APPLICATION_TASK_TAG	0
 
-
 #define configSUPPORT_DYNAMIC_ALLOCATION        1    
-#define configSUPPORT_STATIC_ALLOCATION					1	
+#define configSUPPORT_STATIC_ALLOCATION					0	
 
 /* The full demo always has tasks to run so the tick will never be turned off.
 The blinky demo will use the default tickless idle implementation to turn the
--- Source/UserApp/Main/gpio_led_output.c
+++ Source/UserApp/Main/gpio_led_output.c
@@ -5,20 +5,25 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-#include "FreeRTOS.h"
-#include "Task.h"
+
 #include "board.h"
 #include "fsl_debug_console.h"
 #include "fsl_gpio.h"
 #include "pin_mux.h"
 #include "clock_config.h"
-
 #include "bsp_key.h"
+
+#include "FreeRTOS.h"
+#include "Task.h"
+#include "queue.h"
+#include "semphr.h"
 
 static TaskHandle_t AppTaskCreate_Handle;
 static TaskHandle_t LED_Task_Handle;
 static TaskHandle_t Hello_Task_Handle;
 static TaskHandle_t KEY_Task_Handle;
+
+#if (configSUPPORT_STATIC_ALLOCATION==1)
 /* Task statck*/
 static StackType_t AppTaskCreate_Stack[128];
 static StackType_t LED_Task_Stack[128];
@@ -34,9 +39,10 @@ static StaticTask_t Hello_Task_TCB;
 static StaticTask_t KEY_Task_TCB;
 static StaticTask_t Idle_Task_TCB;
 static StaticTask_t Timer_Task_TCB;
+#endif
 
 /* Task priorities. */
-#define App_Task_PRIORITY    2
+#define App_Task_PRIORITY    6
 #define LED_Task_PRIORITY    3
 #define KEY_Task_PRIORITY    4
 #define Hello_Task_PRIORITY  5
@@ -49,6 +55,14 @@ static void LED_Task(void *pvParameters);
 static void KEY_Task(void *pvParameters);
 
 
+/**********************   Queuue       ***************************************/
+QueueHandle_t Test_Queue = NULL;
+#define QUEUE_LEN  4
+#define QUEUE_SIZE 4
+
+/**********************   Semaphore    **************************************/
+SemaphoreHandle_t  BinarySem_Handle = NULL;
+SemaphoreHandle_t  CountSem_Handle =  NULL;
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
@@ -107,13 +121,22 @@ int main(void)
 		PRINTF("SYSPLLPFD2:      %d Hz\r\n", CLOCK_GetFreq(kCLOCK_SysPllPfd2Clk));
 		PRINTF("SYSPLLPFD3:      %d Hz\r\n", CLOCK_GetFreq(kCLOCK_SysPllPfd3Clk)); 
 		
-	
+#if (configSUPPORT_STATIC_ALLOCATION==1)	
 	  if(xTaskCreate((TaskFunction_t)AppTaskCreate,
 									"AppTaskCreate",
 		               128,
 		               NULL,
 									 App_Task_PRIORITY,
 		               NULL)!=pdPASS)
+#else
+    if(xTaskCreate((TaskFunction_t)AppTaskCreate,
+			             "AppTaskCreate",
+		               512,
+		               NULL,
+		               App_Task_PRIORITY,
+		               (TaskHandle_t*)&AppTaskCreate_Handle
+									)!=pdPASS)
+#endif
 		{
 			PRINTF("Task creation failed!.\r\n");
       while (1);
@@ -124,7 +147,24 @@ int main(void)
 /*************************** task function ************************************/
 static void AppTaskCreate(void)
 {
+	BaseType_t xReturn = pdPASS;
 	taskENTER_CRITICAL();
+	Test_Queue = xQueueCreate(QUEUE_LEN,QUEUE_SIZE);
+	if(NULL!= Test_Queue)
+	{
+		PRINTF("Test_Queue Create OK\r\n");
+	}
+  BinarySem_Handle = xSemaphoreCreateBinary();
+	if(NULL != BinarySem_Handle)
+	{
+		PRINTF("BinarySem_Handle Create OK\r\n");
+	}
+	CountSem_Handle =  xSemaphoreCreateCounting(5,5);
+	if(NULL != CountSem_Handle)
+	{
+		PRINTF("CountSem_Handle Create OK\r\n");
+	}
+#if (configSUPPORT_STATIC_ALLOCATION==1)	
 	LED_Task_Handle = xTaskCreateStatic((TaskFunction_t)LED_Task,
 																			(const char*)"LED_Task",
 																		  128,
@@ -158,18 +198,69 @@ static void AppTaskCreate(void)
 		PRINTF("KEY_Task Create OK\r\n");
 	else
 		PRINTF("KEY_Task Create Failed\r\n");	
-	
+#else
+	 xReturn= xTaskCreate((TaskFunction_t)LED_Task,
+																			(const char*)"LED_Task",
+																		  128,
+																			NULL,
+																			LED_Task_PRIORITY,
+																			&LED_Task_Handle);																	
+	if(pdPASS != xReturn)
+		PRINTF("LED_Task Create OK\r\n");
+	else
+		PRINTF("LED_Task Create Failed\r\n");		
+	xReturn= xTaskCreate((TaskFunction_t)Hello_Task,
+																			(const char*)"Hello_Task",
+																		  128,
+																			NULL,
+																			Hello_Task_PRIORITY,
+																			&Hello_Task_Handle);																					
+	if(pdPASS != xReturn)
+		PRINTF("Hello_Task Create OK\r\n");
+	else
+		PRINTF("Hello_Task Create Failed\r\n");	
+	xReturn= xTaskCreate((TaskFunction_t)KEY_Task,
+																			(const char*)"KEY_Task",
+																		  128,
+																			NULL,
+																			KEY_Task_PRIORITY,
+																			&KEY_Task_Handle);																					
+	if(pdPASS != xReturn)
+		PRINTF("KEY_Task Create OK\r\n");
+	else
+		PRINTF("KEY_Task Create Failed\r\n");	
+#endif
 	vTaskDelete(AppTaskCreate_Handle); 
   taskEXIT_CRITICAL();  
 	//vTaskStartScheduler();	
 }
 static void Hello_Task(void *pvParameters)
 {
-    for (;;)
-    {
-        PRINTF("Hello world.\r\n");
-        vTaskDelay(1000);
-    }
+	BaseType_t xReturn = pdPASS;
+	uint32_t queue_r_data=0xffffffff;
+	PRINTF("Hello world Task Init.\r\n");
+	for (;;)
+	{
+		xReturn =xQueueReceive(Test_Queue,&queue_r_data,portMAX_DELAY);
+		if(pdPASS == xReturn)
+		{
+			PRINTF("Hello Task receive %d from KEY Task\r\n",queue_r_data);
+		}
+		else
+		{
+			PRINTF("Hello Task receive Error\r\n");
+		}
+		xReturn = xSemaphoreTake(BinarySem_Handle,portMAX_DELAY);
+		if(pdPASS == xReturn)
+		{
+			PRINTF("Hello Task get Semphore KEY Task\r\n");
+		}
+		else
+		{
+			PRINTF("Hello Task get Semphore KEY Task Error\r\n");
+		}
+		vTaskDelay(100);
+	}
 }
 
 static void LED_Task(void *pvParameters)
@@ -196,6 +287,7 @@ static void LED_Task(void *pvParameters)
 
 static void KEY_Task(void *pvParameters)
 {
+	BaseType_t xReturn = pdPASS;
 	static unsigned char key_count  =0;
 	for(;;)
 	{
@@ -219,6 +311,16 @@ static void KEY_Task(void *pvParameters)
 				vTaskResume(LED_Task_Handle);
 				PRINTF("Resume LED_Task\r\n");
 			}
+			xReturn = xQueueSend(Test_Queue,&key_count,0);
+			if(pdPASS == xReturn)
+			{
+				PRINTF("Key Pressed send queue to Hello Task %d\r\n",key_count);
+			}
+		  xReturn  = xSemaphoreGive(BinarySem_Handle);
+			if(pdTRUE == xReturn)
+			{
+				PRINTF("Key Pressed send Semaphore\r\n");
+			}
 		}
 		vTaskDelay(10);
 	}
@@ -228,56 +330,20 @@ void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
 																	 StackType_t **ppxIdleTaskStackBuffer, 
 																	 uint32_t *pulIdleTaskStackSize)
 {
+	#if (configSUPPORT_STATIC_ALLOCATION==1)
 	*ppxIdleTaskTCBBuffer=&Idle_Task_TCB;
 	*ppxIdleTaskStackBuffer=Idle_Task_Stack;
 	*pulIdleTaskStackSize=configMINIMAL_STACK_SIZE;
+	#endif
 }
 void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, 
 									StackType_t **ppxTimerTaskStackBuffer, 
 									uint32_t *pulTimerTaskStackSize)
 {
+	#if (configSUPPORT_STATIC_ALLOCATION==1)
 	*ppxTimerTaskTCBBuffer=&Timer_Task_TCB;
 	*ppxTimerTaskStackBuffer=Timer_Task_Stack;
 	*pulTimerTaskStackSize=configTIMER_TASK_STACK_DEPTH;
+	#endif
 }
 
-/*!
- * @brief Main function
- */
-//int main(void)
-//{
-//    /* Define the init structure for the output LED pin*/
-//    gpio_pin_config_t led_config = {kGPIO_DigitalOutput, 0, kGPIO_NoIntmode};
-
-//    /* Board pin, clock, debug console init */
-//    BOARD_ConfigMPU();
-//    BOARD_InitPins();
-//    BOARD_BootClockRUN();
-//    BOARD_InitDebugConsole();
-
-//    /* Print a note to terminal. */
-//    PRINTF("\r\n GPIO Driver example\r\n");
-//    PRINTF("\r\n The LED is blinking.\r\n");
-
-//    /* Init output LED GPIO. */
-//    GPIO_PinInit(EXAMPLE_LED_GPIO, EXAMPLE_LED_GPIO_PIN, &led_config);
-
-//    while (1)
-//    {
-//        SDK_DelayAtLeastUs(100000, SDK_DEVICE_MAXIMUM_CPU_CLOCK_FREQUENCY);
-//#if (defined(FSL_FEATURE_IGPIO_HAS_DR_TOGGLE) && (FSL_FEATURE_IGPIO_HAS_DR_TOGGLE == 1))
-//        GPIO_PortToggle(EXAMPLE_LED_GPIO, 1u << EXAMPLE_LED_GPIO_PIN);
-//#else
-//        if (g_pinSet)
-//        {
-//            GPIO_PinWrite(EXAMPLE_LED_GPIO, EXAMPLE_LED_GPIO_PIN, 0U);
-//            g_pinSet = false;
-//        }
-//        else
-//        {
-//            GPIO_PinWrite(EXAMPLE_LED_GPIO, EXAMPLE_LED_GPIO_PIN, 1U);
-//            g_pinSet = true;
-//        }
-//#endif /* FSL_FEATURE_IGPIO_HAS_DR_TOGGLE */
-//    }
-//}
